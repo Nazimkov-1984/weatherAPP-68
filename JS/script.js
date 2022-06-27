cfbac39dfcfdc5fcd762b4aba3fe2e9f52c50215
Nazimkov-1984 <nazimkoff@gmail.com>
2022-06-27 14:01:03 +0300

    debugging

--- JS/script.js
+++ JS/script.js
@@ -14,15 +14,19 @@ setInterval(() => {
     1,
     7
   )}.jpg)`;
-}, 5000);
+}, 10000);
+
+const loader = document.querySelector(".lds-spinner");
+loader.style.display = "none";
 
 //слушаталь событий - повесить слушатель события на элемент
 button.addEventListener("click", (event) => {
   if (input.value !== "") {
+    loader.style.display = "block";
+    button.style.disabled = "true";
     // запрос на сервер
     const city_name = input.value;
     const API_key = "0e3f4c3098c7d2107ce581907ae44eb7";
-
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_key}&units=metric`;
     fetch(url)
       .then((res) => res.json())
@@ -35,9 +39,34 @@ button.addEventListener("click", (event) => {
           data.main.temp
         ).toFixed()} градусів`;
         temp.style.backgroundColor = "white";
+        const toBeTemp = results.querySelector("div");
+        if (toBeTemp) {
+          results.removeChild(toBeTemp);
+        }
         results.appendChild(temp);
+        loader.style.display = "none";
+        button.style.disabled = "false";
+      })
+      .catch(() => {
+        const results = document.querySelector(".results");
+        const temp = document.createElement("div");
+        temp.textContent = "Місто не знайдено";
+        temp.style.backgroundColor = "white";
+        const toBeTemp = results.querySelector("div");
+        if (toBeTemp) {
+          results.removeChild(toBeTemp);
+        }
+        results.appendChild(temp);
+        input.classList.add("input_error");
+        loader.style.display = "none";
+        flag.style.backgroundImage = "none";
       });
   } else {
-    input.classList.toggle("input_error");
+    input.classList.add("input_error");
   }
 });
+
+input.addEventListener("click", (e) => {
+  input.classList.remove("input_error");
+});
+
